6a148e1c17cd28d1dce53734e063e9fa1da76611
LAPTOP-J6F0OR4J\TSelmeg <99040437@mydavinci.nl>
2018-02-21 09:12:55 +0100

    css upgrade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        @if(Auth::user()->role == 'admin')<li><a href="{{url('/admin/')}}">Admin</a></li>@endif 
+                        @if(Auth::check() && Auth::user()->role == 'admin')<li><a href="{{url('/admin/')}}">Admin</a></li>@endif 
                     </ul>
 
                     <!-- Right Side Of Navbar -->
